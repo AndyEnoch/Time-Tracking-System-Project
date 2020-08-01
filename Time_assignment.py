803c5f994c62308d37bb94267066b1f76bbe8d13
Stephanie Dawsonn-Andoh <stephandoh@gmail.com>
2020-08-01 11:50:38 +0000

    rate pay

--- Time_assignment.py
+++ Time_assignment.py
@@ -19,3 +19,15 @@ while True:
 print(end_date)
 
 
+total_seconds=work_time.seconds
+    #print('tot sec: ', total_seconds)
+hours = total_seconds/3600
+    #print('tot hours: ', hours)
+hours_days = work_time.days
+    #print('tot hoursdays: ', hours_days)
+total_hours = hours + (hours_days * 24)
+    #print('tot_hours: ', total_hours)
+rate_of_pay = 5
+total_pay = round(total_hours * rate_of_pay, 2)
+print(f'Your total pay is {currency}', total_pay)
+today = date.today()
